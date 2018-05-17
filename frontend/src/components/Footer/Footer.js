aa8c377e5fbf7c7dd9d0eb820b0a337ad2d37c08
Antonio Campos <programmer.tony@gmail.com>
2018-05-16 22:09:45 -0500

    #17 - Footer component

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import './Footer.css';
+import LinkedInLogo from './linkedin.svg';
 
 class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <div className="footer">
+        <a href="https://www.linkedin.com/in/jesus-campos/" target="_blank">
+          <img src={LinkedInLogo} className="profile"/>
+        </a>
+      </div>
     )
   }
 }
